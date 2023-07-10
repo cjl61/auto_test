f0e9381357200a527f0d290cac864b3ecdbb193b
cjl61 <53484421+cjl61@users.noreply.github.com>
2023-07-10 20:35:33 +0800

    Delete common directory
    2

--- common/common_fun.py
+++
@@ -1,9 +0,0 @@
-from selenium.common.exceptions import NoSuchElementException
-from common.get_driver import android_driver
-from selenium.webdriver.common.by import By
-
-driver = android_driver()
-def check_agree_into_app():
-
-    driver.find_element(By.ID,'com.ccbhome.lanhai:id/btn_sure')
-
